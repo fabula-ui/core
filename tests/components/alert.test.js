3b1043432a165f6bc1bb14c87bfae5468f4c1c01
Bruno Poeta <brunovieirapoeta@gmail.com>
2020-06-30 12:02:17 -0300

    Updating tests

--- tests/components/alert.test.js
+++ tests/components/alert.test.js
@@ -2,11 +2,40 @@ describe('Alert', () => {
     const port = process.env.PORT || '9009';
     beforeAll(async () => {
         jest.setTimeout(100000);
-        page.setViewport({ width: 1920, height: 1080 });
+        // page.setViewport({ width: 1920, height: 1080 });
+    });
+
+    it('border', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
+        await page.goto(`http://localhost:${port}/iframe.html?id=alert--border`, { waitUntil: 'load', timeout: 10000 });
+
+        const image = await page.screenshot();
+
+        expect(image).toMatchImageSnapshot({
+            failureThreshold: 0.01,
+            failureThresholdType: 'percent'
+        });
     });
 
     it('clear', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--clear`, { waitUntil: 'load', timeout: 10000 });
+
+        const image = await page.screenshot();
+
+        expect(image).toMatchImageSnapshot({
+            failureThreshold: 0.01,
+            failureThresholdType: 'percent'
+        });
+    });
+
+    it('close button', async () => {
+        page.setViewport({ width: 1200, height: 100 });
+
+        await page.goto(`http://localhost:${port}/iframe.html?id=alert--close-button`, { waitUntil: 'load', timeout: 10000 });
+
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
@@ -16,7 +45,10 @@ describe('Alert', () => {
     });
 
     it('color', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--color`, { waitUntil: 'load', timeout: 10000 });
+
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
@@ -26,7 +58,10 @@ describe('Alert', () => {
     });
 
     it('custom content', async () => {
+        page.setViewport({ width: 640, height: 100 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--custom-content`, { waitUntil: 'load', timeout: 10000 });
+
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
@@ -36,7 +71,10 @@ describe('Alert', () => {
     });
 
     it('faded', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--faded`, { waitUntil: 'load', timeout: 10000 });
+
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
@@ -46,7 +84,10 @@ describe('Alert', () => {
     });
 
     it('glow', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--glow`, { waitUntil: 'load', timeout: 10000 });
+
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
@@ -56,7 +97,11 @@ describe('Alert', () => {
     });
 
     it('icons', async () => {
+        page.setViewport({ width: 1200, height: 350 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--icons`, { waitUntil: 'load', timeout: 10000 });
+        await page.waitFor(1000);
+
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
@@ -66,7 +111,11 @@ describe('Alert', () => {
     });
 
     it('icon colors', async () => {
+        page.setViewport({ width: 1200, height: 350 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--icon-colors`, { waitUntil: 'load', timeout: 10000 });
+        await page.waitFor(1000);
+
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
@@ -76,6 +125,8 @@ describe('Alert', () => {
     });
 
     it('invert', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--invert`, { waitUntil: 'load', timeout: 10000 });
         const image = await page.screenshot();
 
@@ -86,6 +137,8 @@ describe('Alert', () => {
     });
 
     it('marker', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--marker`, { waitUntil: 'load', timeout: 10000 });
         const image = await page.screenshot();
 
@@ -96,6 +149,8 @@ describe('Alert', () => {
     });
 
     it('outline', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--outline`, { waitUntil: 'load', timeout: 10000 });
         const image = await page.screenshot();
 
@@ -106,7 +161,21 @@ describe('Alert', () => {
     });
 
     it('text color', async () => {
+        page.setViewport({ width: 1200, height: 300 });
+
         await page.goto(`http://localhost:${port}/iframe.html?id=alert--text-color`, { waitUntil: 'load', timeout: 10000 });
+        const image = await page.screenshot();
+
+        expect(image).toMatchImageSnapshot({
+            failureThreshold: 0.01,
+            failureThresholdType: 'percent'
+        });
+    });
+
+    it('title color', async () => {
+        page.setViewport({ width: 1200, height: 350 });
+
+        await page.goto(`http://localhost:${port}/iframe.html?id=alert--title-color`, { waitUntil: 'load', timeout: 10000 });
         const image = await page.screenshot();
 
         expect(image).toMatchImageSnapshot({
