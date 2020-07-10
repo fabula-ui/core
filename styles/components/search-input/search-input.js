4183ed4c52fca2f7ed524d1ccdc83aa1b990204f
Bruno Poeta <brunovieirapoeta@gmail.com>
2020-07-10 11:30:36 -0300

    Search input styles refactored

--- styles/components/search-input/search-input.js
+++ styles/components/search-input/search-input.js
@@ -1,17 +1,8 @@
-// Methods
-import getComponentVars from '../../methods/misc/getComponentVars';
-
-const SearchInputStyles = params => {
-    const { framework, props } = params;
-    const vars = getComponentVars('input');
-    const wrapper = framework === 'angular' ? '.fab-search-input-wrapper' : '&';
-
-    return `
+const SearchInputStyles = () => `
         .fab-search-input {
             font-size: 1em;
             position: relative;
         }
     `
-}
 
 export default SearchInputStyles;
