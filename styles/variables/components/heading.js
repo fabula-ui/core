7a3aa88ee37e7386e80e15e983bff8c0021ae058
Bruno Poeta <bruno.poeta@toptal.com>
2020-07-23 20:12:35 -0300

    Changed heading component size multipliers

--- styles/variables/components/heading.js
+++ styles/variables/components/heading.js
@@ -9,11 +9,11 @@ const heading = {
     letterSpacing: '-.05em',
     levelMultipliers: {
         '1': 3,
-        '2': 2,
-        '3': 1.5,
-        '4': 1.25,
-        '5': 1.1,
-        '6': 1
+        '2': 2.5,
+        '3': 2,
+        '4': 1.5,
+        '5': 1.25,
+        '6': 1.1
     },
     marginBottom: '1em',
     marginTop: '1.5em'
