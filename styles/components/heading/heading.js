6921fef09d633a956c0bc47e0e1e548279c99a65
Bruno Poeta <bruno.poeta@toptal.com>
2020-09-30 12:11:40 -0300

    Changes on heading element to make margins work on angular

--- styles/components/heading/heading.js
+++ styles/components/heading/heading.js
@@ -10,17 +10,21 @@ const HeadingStyles = params => {
     const wrapper = framework === 'angular' ? '.fab-heading' : '&';
 
     return `
-        ${wrapper} {
-            font-family: ${vars.fontFamily};
-            font-size: calc(1rem * ${vars.levelMultipliers[props.level]});
-            font-weight: ${props.weight || vars.fontWeight};
-            letter-spacing: ${vars.letterSpacing};
+        & {
             margin-bottom: ${vars.marginBottom};
             margin-top: 0;
 
             &:not(:first-child) {
                 margin-top: ${vars.marginTop};
             }
+        }
+
+        ${wrapper} {
+            font-family: ${vars.fontFamily};
+            font-size: calc(1rem * ${vars.levelMultipliers[props.level]});
+            font-weight: ${props.weight || vars.fontWeight};
+            letter-spacing: ${vars.letterSpacing};
+            margin: 0;
         }
 
         // Modifiers
