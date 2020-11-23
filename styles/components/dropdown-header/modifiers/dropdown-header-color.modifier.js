443ecb0ae458899b65da4333b9b1107020766d5b
Bruno Poeta <brunovieirapoeta@gmail.com>
2020-11-23 01:02:24 -0300

    Removing unused dropdown header props

--- styles/components/dropdown-header/modifiers/dropdown-header-color.modifier.js
+++ styles/components/dropdown-header/modifiers/dropdown-header-color.modifier.js
@@ -1,4 +1,3 @@
-import getBgColor from '../../../methods/color/getBgColor';
 import getColor from '../../../methods/color/getColor';
 import getComponentVars from '../../../methods/misc/getComponentVars';
 import getTextColor from '../../../methods/color/getTextColor';
@@ -7,7 +6,6 @@ const colorModifier = params => {
     const { framework, props } = params;
     const vars = getComponentVars('dropdownHeader');
     const color = (props.color || props.parentColor) && getColor(props.color || props.parentColor, vars.colors);
-    const bgColor = props.color ? getBgColor(color, 'fill') : vars.color;
     const wrapper = framework === 'angular' ? '.fab-dropdown-header' : '&';
 
     let textColor = (props.color || props.parentColor) ? getTextColor(color, 'fill') : vars.textColor;
