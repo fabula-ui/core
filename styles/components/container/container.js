96f93ec27f16379dc40a05b2a1eea60f82d0b3b9
Bruno Poeta <brunovieirapoeta@gmail.com>
2020-11-25 12:37:53 -0300

    Add fluid prop to container component

--- styles/components/container/container.js
+++ styles/components/container/container.js
@@ -1,6 +1,5 @@
 const ContainerStyles = params => {
     const { framework, props } = params;
-    const wrapper = framework === 'angular' ? '.fab-container' : '&';
 
     return `
         & {
@@ -9,23 +8,26 @@ const ContainerStyles = params => {
             margin-right: auto;
             padding-left: 15px;
             padding-right: 15px;
-            width: 100%;
+            ${props.fluid ? `width: 100%;` : ''}
 
-            @media (min-width: 576px) {
-                max-width: 540px;
-            }
-            
-            @media (min-width: 768px) {
-                max-width: 720px;
-            }
+            ${!props.fluid ?
+            `
+                @media (min-width: 576px) {
+                    max-width: 540px;
+                }
+                
+                @media (min-width: 768px) {
+                    max-width: 720px;
+                }
 
-            @media (min-width: 992px) {
-                max-width: 960px;
-            }
+                @media (min-width: 992px) {
+                    max-width: 960px;
+                }
 
-            @media (min-width: 1200px) {
-                max-width: 1140px;
-            }
+                @media (min-width: 1200px) {
+                    max-width: 1140px;
+                }
+                ` : ''}
         }
 
         & > .fab-container {
